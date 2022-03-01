d6cfa1019c98b09d61e38260fc71912ec7c3a08a
jahid4striver <jahidhassn701@gmail.comr>
2022-03-02 02:45:49 +0600

    Error Handle and 20 Item Showed

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 const searchField= document.getElementById('search-field');
 const notFound= document.getElementById('not-found');
 const phoneGrid= document.getElementById('phone-grid');
+const detailsDiv= document.getElementById('phone-details');
+
 
 
 // Dynamic Search Function
@@ -11,7 +13,7 @@ const searchResult= ()=>{
     const inputValue=searchField.value;
     fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
     .then(res=> res.json())
-    .then(data=> displayResult(data.data))
+    .then(data=> displayResult(data.data.slice(0,20)))
     searchField.value='';
 }
 
@@ -22,8 +24,13 @@ const displayResult= phones=> {
     if(phones==''){
         notFound.classList.replace('d-none', 'd-block');
         phoneGrid.innerText='';
+        detailsDiv.innerHTML='';
     }else{
+        notFound.classList.replace('d-block', 'd-none');
+        phoneGrid.innerText='';
+        detailsDiv.innerText='';
         phones.forEach(phone => {
+        console.log(phone);
            const  phoneDiv= document.createElement('div');
            phoneDiv.classList.add('col-lg-4');
            phoneDiv.classList.add('col-md-6');
@@ -45,7 +52,6 @@ const displayResult= phones=> {
 
 // Loading Phone Details
 
-const detailsDiv= document.getElementById('phone-details');
 
 const phoneDetails= (phoneSlug)=>{
     const url=(`https://openapi.programming-hero.com/api/phone/${phoneSlug}`);
@@ -63,28 +69,27 @@ const showDetails= (phone)=>{
     div.classList.add('col-md-6');
     div.classList.add('mx-auto');
     div.innerHTML=`<div class="card h-100 rounded shadow-lg p-4">
+    <h1 class="text-center text-success fw-bold">${phone.name}</h1>
     <img src="${phone.image}" class="w-100 mx-auto card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">Name: ${phone.name}</h5>
         <p class="card-text">Brand: ${phone.brand}</p>
-        <p class="card-text">Release Date: ${phone.releaseDate}</p>
+        <p class="card-text">Release Date: ${phone?.releaseDate||'No Release Date Found'}</p>
         <h5 class="card-title">Main Features:</h5>
         <p class="card-text">Chipset: ${phone.mainFeatures.chipSet}</p>
         <p class="card-text">Display Size: ${phone.mainFeatures.displaySize}</p>
         <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
         <p class="card-text">Sensors: ${phone.mainFeatures.sensors[0]} ${phone.mainFeatures.sensors[1]}, ${phone.mainFeatures.sensors[2]}, ${phone.mainFeatures.sensors[3]}, ${phone.mainFeatures.sensors[4]}, ${phone.mainFeatures.sensors[5]}, ${phone.mainFeatures.sensors[6]} </p>
         <h5 class="card-title">Others Features:</h5>
-        <p class="card-text">Bluetooth: ${phone.others.Bluetooth}</p>
-        <p class="card-text">GPS: ${phone.others.GPS}</p>
-        <p class="card-text">NFC: ${phone.others.NFC}</p>        
-        <p class="card-text">Radio: ${phone.others.Radio}</p>
-        <p class="card-text">USB: ${phone.others.USB}</p>
-        <p class="card-text">WLAN: ${phone.others.WLAN}</p>
+        <p class="card-text">Bluetooth: ${phone?.others?.Bluetooth||'Info Not Available'}</p>
+        <p class="card-text">GPS: ${phone?.others?.GPS||'Info Not Available'}</p>
+        <p class="card-text">NFC: ${phone?.others?.NFC||'Info Not Available'}</p>        
+        <p class="card-text">Radio: ${phone?.others?.Radio||'Info Not Available'}</p>
+        <p class="card-text">USB: ${phone?.others?.USB||'Info Not Available'}</p>
+        <p class="card-text">WLAN: ${phone?.others?.WLAN||'Info Not Available'}</p>
     </div>
 </div>`;
 
 detailsDiv.appendChild(div);
 }
-
-// ${phone.sensors[1]}, ${phone.sensors[2]}, ${phone.sensors[3]}, ${phone.sensors[4]}, ${phone.sensors[5]}
 
